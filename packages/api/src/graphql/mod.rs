d3c39d2b2aa7faccc27da40096fd91fc9d4c5bf1
Cedric Vangout <mail@cvng.dev>
2021-08-19 23:32:30 -0400

    feat(api): setup graphql (#1)

---
+++ packages/api/src/graphql/mod.rs
@@ -0,0 +1,14 @@
+mod query;
+
+use async_graphql::EmptyMutation;
+use async_graphql::EmptySubscription;
+use async_graphql::Schema;
+use query::Query;
+
+/// Piteo GraphQL schema.
+pub type PiteoSchema = Schema<Query, EmptyMutation, EmptySubscription>;
+
+/// Build Piteo GraphQL schema. https://async-graphql.github.io
+pub fn build_schema() -> PiteoSchema {
+    Schema::build(Query, EmptyMutation, EmptySubscription).finish()
+}
