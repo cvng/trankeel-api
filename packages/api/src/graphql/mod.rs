b0a2d28454996a494e4e28d5eef962bedce389a2
Cedric Vangout <mail@cvng.dev>
2021-08-21 03:35:08 -0400

    feat(api): add auth_id fallback

--- packages/api/src/graphql/mod.rs
+++ packages/api/src/graphql/mod.rs
@@ -5,11 +5,14 @@ use crate::graphql::query::Query;
 use async_graphql::EmptyMutation;
 use async_graphql::EmptySubscription;
 use async_graphql::Schema;
+use piteo_core::auth_id_fallback_from_env;
 
 /// Piteo GraphQL schema.
 pub type PiteoSchema = Schema<Query, EmptyMutation, EmptySubscription>;
 
 /// Build Piteo GraphQL schema. https://async-graphql.github.io
 pub fn build_schema() -> PiteoSchema {
-    Schema::build(Query, EmptyMutation, EmptySubscription).finish()
+    Schema::build(Query, EmptyMutation, EmptySubscription)
+        .data(auth_id_fallback_from_env())
+        .finish()
 }
