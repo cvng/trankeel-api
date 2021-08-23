3725ac933f4da98a0b9fece8cd258fab6d457ed9
Cedric Vangout <mail@cvng.dev>
2021-08-23 03:55:43 -0400

    feat(core): update Result type (#8)

--- packages/core/src/lib.rs
+++ packages/core/src/lib.rs
@@ -2,15 +2,15 @@
 extern crate diesel;
 
 mod database;
+mod error;
 mod schema;
 
 pub mod auth;
-pub mod error;
 pub mod properties;
 
 pub use crate::auth::AuthId;
 pub use crate::auth::Person;
 pub use crate::database::build_connection_pool;
 pub use crate::database::DbPool;
-pub use crate::error::eyre::Result;
+pub use crate::error::Error;
 pub use crate::properties::Property;
