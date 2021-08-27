c03edc312a1ca427d07e8dd57e886107389f68e6
Cedric Vangout <mail@cvng.dev>
2021-08-26 23:59:27 -0400

    feat(web): add run command (#15)

--- packages/web/src/web.rs
+++ packages/web/src/web.rs
@@ -1,5 +1,7 @@
+use std::env;
 use std::process::Command;
 
 fn main() {
-  Command::new("npx").arg("next").arg("start").status().ok();
+    env::set_current_dir(&env::var("CARGO_MANIFEST_DIR").unwrap()).ok();
+    Command::new("npx").arg("next").arg("start").status().ok();
 }
