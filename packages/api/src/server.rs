7362ee9735178e1b9d73ee085bb05e26ef06d3aa
Cedric Vangout <mail@cvng.dev>
2021-08-28 03:44:27 -0400

    feat(api): add rocket_cors (#17)

--- packages/api/src/server.rs
+++ packages/api/src/server.rs
@@ -8,12 +8,17 @@ use rocket::Rocket;
 
 /// Build Piteo Web server. https://rocket.rs
 pub fn server() -> Result<Rocket<Build>> {
+    let cors = rocket_cors::CorsOptions::default().to_cors()?;
+
     let schema = piteo_graphql::build_schema()?;
 
-    let server = rocket::build().manage(schema).mount(
-        "/",
-        routes![graphql_playground, graphql_query, graphql_request],
-    );
+    let server = rocket::build()
+        .manage(schema)
+        .mount(
+            "/",
+            routes![graphql_playground, graphql_query, graphql_request],
+        )
+        .attach(cors);
 
     Ok(server)
 }
