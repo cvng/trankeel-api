2c69e51c3e29f8988581f9485f9f2ec2e62986d9
Cedric Vangout <mail@cvng.dev>
2021-09-04 02:03:48 -0400

    move op

--- packages/core/src/payment.rs
+++ packages/core/src/payment.rs
@@ -7,19 +7,13 @@ type Id = String;
 
 pub type CustomerId = String;
 
-pub struct Customer {
-    pub id: Id,
-    pub email: Email,
-}
-
 pub struct Subscription {
     pub id: Id,
+    pub customer_id: CustomerId,
     pub status: AccountStatus,
     pub trial_end: DateTime,
 }
 
 pub trait PaymentProvider {
-    fn create_customer(&self, email: Email) -> Result<Customer, Error>;
-
-    fn create_subscription(&self, customer_id: CustomerId) -> Result<Subscription, Error>;
+    fn create_subscription(&self, email: Email) -> Result<Subscription, Error>;
 }
