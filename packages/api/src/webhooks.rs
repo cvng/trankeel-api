f77674dbba8944958296d3b6bb60db95bf57c0a3
Cedric Vangout <mail@cvng.dev>
2021-09-22 01:55:47 -0400

    Update webhooks.rs

--- packages/api/src/webhooks.rs
+++ packages/api/src/webhooks.rs
@@ -1,11 +1,13 @@
 use piteo::database::Db;
 use piteo::leases::SendReceiptsInput;
 use piteo::pdfmaker::Document;
+use piteo::DbPool;
 use piteo::FileData;
 use piteo::FileStatus;
 use piteo::FileType;
 use piteo::Pg;
 use piteo::Provider;
+use piteo::Receipt;
 use rocket::http::Status;
 use rocket::info;
 use rocket::post;
@@ -23,11 +25,12 @@ pub struct PdfmonkeyPayload {
 pub async fn pdfmonkey_request(request: Json<PdfmonkeyPayload>) -> Status {
     info!("Received pdfmonkey request: {:?}", request);
 
-    let db_pool = &Pg::init().inner();
-    let db = piteo::db(db_pool);
+    let db_pool = Pg::init().inner();
+    let db = piteo::db(&db_pool);
 
     let document = request.document.clone();
 
+    // General processing for any document.
     let file = db.files().by_external_id(&document.id).unwrap();
     let file = db
         .files()
@@ -40,20 +43,32 @@ pub async fn pdfmonkey_request(request: Json<PdfmonkeyPayload>) -> Status {
         })
         .unwrap();
 
+    // If not success, stop processing.
     if file.status != Some(FileStatus::Success) {
         warn!("Document errors: {:?}", document.errors);
-        return Status::Ok; // If not success, stop processing.
+        return Status::Ok;
     }
 
+    // Specific processing by document type.
     match file.type_ {
         FileType::RentReceipt => {
-            let rent = db.rents().by_receipt_id(&file.id).unwrap();
-            let input = SendReceiptsInput {
-                rent_ids: vec![rent.id],
-            };
-            piteo::send_receipts(db_pool, input).await.unwrap();
-            Status::Ok
+            on_receipt_created(&db_pool, &file).await.ok();
         }
         _ => panic!(),
     }
+
+    Status::Ok
+}
+
+// # Handlers
+
+async fn on_receipt_created(db_pool: &DbPool, receipt: &Receipt) {
+    let db = piteo::db(&db_pool);
+
+    let rent = db.rents().by_receipt_id(&receipt.id).unwrap();
+    let input = SendReceiptsInput {
+        rent_ids: vec![rent.id],
+    };
+
+    piteo::send_receipts(&db_pool, input).await.ok();
 }
