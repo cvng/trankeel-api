58dbd727c1ab620dbbe1bb91f63993b06f2c4f8d
Cedric Vangout <mail@cvng.dev>
2021-09-28 14:18:18 -0400

    Update up.sql

--- migrations/20210927184520_create_reports/up.sql
+++ migrations/20210927184520_create_reports/up.sql
@@ -46,11 +46,11 @@ summary_5 AS (
         100.0 * amount_partial / amount_expected AS ratio_partial,
         100.0 * amount_pending / amount_expected AS ratio_pending,
         --
-        0.0 AS variation_expected, -- TODO
-        0.0 AS variation_received, -- TODO
-        0.0 AS variation_settled, -- TODO
-        0.0 AS variation_partial, -- TODO
-        0.0 AS variation_pending, -- TODO
+        0 AS variation_expected, -- TODO
+        0 AS variation_received, -- TODO
+        0 AS variation_settled, -- TODO
+        0 AS variation_partial, -- TODO
+        0 AS variation_pending, -- TODO
         --
         100.0 * n_received / n_expected AS payment_rate,
         100.0 * n_units_rented / n_units_owned AS occupation_rate
@@ -60,30 +60,30 @@ SELECT
     account_id,
     current_month,
     --
-    0.0 amount_expected,
-    0.0 amount_received,
-    0.0 amount_settled,
-    0.0 amount_partial,
-    0.0 amount_pending,
+    coalesce(amount_expected, 0)::numeric amount_expected,
+    coalesce(amount_received, 0)::numeric amount_received,
+    coalesce(amount_settled, 0)::numeric amount_settled,
+    coalesce(amount_partial, 0)::numeric amount_partial,
+    coalesce(amount_pending, 0)::numeric amount_pending,
     --
-    0 AS n_expected,
-    0 AS n_received,
-    0 AS n_settled,
-    0 AS n_partial,
-    0 AS n_pending,
+    coalesce(n_expected, 0)::integer AS n_expected,
+    coalesce(n_received, 0)::integer AS n_received,
+    coalesce(n_settled, 0)::integer AS n_settled,
+    coalesce(n_partial, 0)::integer AS n_partial,
+    coalesce(n_pending, 0)::integer AS n_pending,
     --
-    0.0 AS ratio_expected,
-    0.0 AS ratio_received,
-    0.0 AS ratio_settled,
-    0.0 AS ratio_partial,
-    0.0 AS ratio_pending,
+    coalesce(ratio_expected, 0)::float AS ratio_expected,
+    coalesce(ratio_received, 0)::float AS ratio_received,
+    coalesce(ratio_settled, 0)::float AS ratio_settled,
+    coalesce(ratio_partial, 0)::float AS ratio_partial,
+    coalesce(ratio_pending, 0)::float AS ratio_pending,
     --
-    0.0 AS variation_expected,
-    0.0 AS variation_received,
-    0.0 AS variation_settled,
-    0.0 AS variation_partial,
-    0.0 AS variation_pending,
+    coalesce(variation_expected, 0)::float AS variation_expected,
+    coalesce(variation_received, 0)::float AS variation_received,
+    coalesce(variation_settled, 0)::float AS variation_settled,
+    coalesce(variation_partial, 0)::float AS variation_partial,
+    coalesce(variation_pending, 0)::float AS variation_pending,
     --
-    0.0 AS payment_rate,
-    0.0 AS occupation_rate
+    coalesce(payment_rate, 0)::float AS payment_rate,
+    coalesce(occupation_rate, 0)::float AS occupation_rate
 FROM summary_1, summary_2, summary_3, summary_4, summary_5;
