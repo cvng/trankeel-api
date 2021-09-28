c6edd0d7aef819c12e00a39be43a9e2cca577ec4
Cedric Vangout <mail@cvng.dev>
2021-09-27 23:56:33 -0400

    Update summary.rs

--- packages/data/src/summary.rs
+++ packages/data/src/summary.rs
@@ -1,46 +1,73 @@
+use crate::AccountId;
 use crate::Amount;
 use crate::DateTime;
+use diesel::sql_types::*;
 
 // # Types
 
 /// Synthesis of collected rents.
+#[derive(Clone, Debug, QueryableByName)]
 pub struct Summary {
-    pub since: DateTime,
-    pub until: DateTime,
+    #[sql_type = "Uuid"]
+    pub account_id: AccountId,
+    #[sql_type = "Timestamptz"]
+    pub current_month: DateTime,
     //
+    #[sql_type = "Numeric"]
     pub amount_expected: Amount,
+    #[sql_type = "Numeric"]
     pub amount_received: Amount,
+    #[sql_type = "Numeric"]
     pub amount_settled: Amount,
+    #[sql_type = "Numeric"]
     pub amount_partial: Amount,
+    #[sql_type = "Numeric"]
     pub amount_pending: Amount,
     //
-    pub n_expected: u32,
-    pub n_received: u32,
-    pub n_settled: u32,
-    pub n_partial: u32,
-    pub n_pending: u32,
+    #[sql_type = "Int4"]
+    pub n_expected: i32,
+    #[sql_type = "Int4"]
+    pub n_received: i32,
+    #[sql_type = "Int4"]
+    pub n_settled: i32,
+    #[sql_type = "Int4"]
+    pub n_partial: i32,
+    #[sql_type = "Int4"]
+    pub n_pending: i32,
     //
+    #[sql_type = "Float8"]
     pub ratio_expected: f64,
+    #[sql_type = "Float8"]
     pub ratio_received: f64,
+    #[sql_type = "Float8"]
     pub ratio_settled: f64,
+    #[sql_type = "Float8"]
     pub ratio_partial: f64,
+    #[sql_type = "Float8"]
     pub ratio_pending: f64,
     //
+    #[sql_type = "Float8"]
     pub variation_expected: f64,
+    #[sql_type = "Float8"]
     pub variation_received: f64,
+    #[sql_type = "Float8"]
     pub variation_settled: f64,
+    #[sql_type = "Float8"]
     pub variation_partial: f64,
+    #[sql_type = "Float8"]
     pub variation_pending: f64,
     //
+    #[sql_type = "Float8"]
     pub payment_rate: f64,
+    #[sql_type = "Float8"]
     pub occupation_rate: f64,
 }
 
 impl Default for Summary {
     fn default() -> Self {
         Self {
-            since: DateTime::default(),
-            until: DateTime::default(),
+            account_id: Default::default(),
+            current_month: DateTime::default(),
             amount_expected: Default::default(),
             amount_received: Default::default(),
             amount_settled: Default::default(),
