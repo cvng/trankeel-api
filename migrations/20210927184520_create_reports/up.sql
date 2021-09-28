207f69039faa62ad2b38b8fb0ca247f5fdbf69f4
Cedric Vangout <mail@cvng.dev>
2021-09-27 22:17:09 -0400

    Update up.sql

--- migrations/20210927184520_create_reports/up.sql
+++ migrations/20210927184520_create_reports/up.sql
@@ -2,16 +2,22 @@ CREATE OR REPLACE VIEW reports AS
 WITH
 summary_1 AS (
     SELECT
-        3000 AS amount_expected,
-        1000 AS amount_settled,
-        500 AS amount_partial,
+        accounts.id AS account_id,
+        date_trunc('month', current_timestamp) AS current_month,
         --
-        3 AS n_expected,
-        1 AS n_settled,
-        1 AS n_partial,
+        sum(full_amount) AS amount_expected,
+        sum(full_amount) FILTER (WHERE rents.status = 'settled') AS amount_settled,
+        sum(full_amount) FILTER (WHERE rents.status = 'partial') AS amount_partial,
         --
-        3 AS n_units_owned,
-        3 AS n_units_rented
+        count(rents.id) AS n_expected,
+        count(rents.id) FILTER (WHERE rents.status = 'settled') AS n_settled,
+        count(rents.id) FILTER (WHERE rents.status = 'partial') AS n_partial
+    FROM rents
+    LEFT JOIN leases ON leases.id = rents.lease_id
+    LEFT JOIN accounts ON accounts.id = leases.account_id
+    AND date_trunc('month', period_start) = date_trunc('month', current_timestamp)
+    GROUP BY accounts.id
+    HAVING accounts.id IS NOT NULL
 ),
 summary_2 AS (
     SELECT
@@ -24,10 +30,17 @@ summary_3 AS (
         amount_expected - amount_received AS amount_pending,
         n_expected - n_received AS n_pending
     FROM summary_1, summary_2
+),
+summary_4 AS (
+    SELECT
+        count(properties.id) AS n_units_owned,
+        count(properties.id) FILTER (WHERE properties.status = 'rented') AS n_units_rented
+    FROM summary_1
+    LEFT JOIN properties ON properties.account_id = summary_1.account_id
 )
 SELECT
-    current_timestamp AS since,
-    current_timestamp AS until,
+    account_id,
+    current_month,
     --
     amount_expected,
     amount_received,
@@ -55,4 +68,4 @@ SELECT
     --
     100.0 * n_received / n_expected AS payment_rate,
     100.0 * n_units_rented / n_units_owned AS occupation_rate
-FROM summary_1, summary_2, summary_3;
+FROM summary_1, summary_2, summary_3, summary_4;
