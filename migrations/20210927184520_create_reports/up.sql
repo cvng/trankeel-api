7af35d1e2f309e4f8c0e8f89b1740ddde297f6b0
Cedric Vangout <mail@cvng.dev>
2021-09-28 03:38:45 -0400

    Update up.sql

--- migrations/20210927184520_create_reports/up.sql
+++ migrations/20210927184520_create_reports/up.sql
@@ -37,35 +37,53 @@ summary_4 AS (
         count(properties.id) FILTER (WHERE properties.status = 'rented') AS n_units_rented
     FROM summary_1
     LEFT JOIN properties ON properties.account_id = summary_1.account_id
+),
+summary_5 AS (
+    SELECT
+        100.0 * amount_expected / amount_expected AS ratio_expected,
+        100.0 * amount_received / amount_expected AS ratio_received,
+        100.0 * amount_settled / amount_expected AS ratio_settled,
+        100.0 * amount_partial / amount_expected AS ratio_partial,
+        100.0 * amount_pending / amount_expected AS ratio_pending,
+        --
+        0.0 AS variation_expected, -- TODO
+        0.0 AS variation_received, -- TODO
+        0.0 AS variation_settled, -- TODO
+        0.0 AS variation_partial, -- TODO
+        0.0 AS variation_pending, -- TODO
+        --
+        100.0 * n_received / n_expected AS payment_rate,
+        100.0 * n_units_rented / n_units_owned AS occupation_rate
+    FROM summary_1, summary_2, summary_3, summary_4
 )
 SELECT
     account_id,
     current_month,
     --
-    amount_expected,
-    amount_received,
-    amount_settled,
-    amount_partial,
-    amount_pending,
+    0.0 amount_expected,
+    0.0 amount_received,
+    0.0 amount_settled,
+    0.0 amount_partial,
+    0.0 amount_pending,
     --
-    n_expected,
-    n_received,
-    n_settled,
-    n_partial,
-    n_pending,
+    0 AS n_expected,
+    0 AS n_received,
+    0 AS n_settled,
+    0 AS n_partial,
+    0 AS n_pending,
     --
-    100.0 * amount_expected / amount_expected AS ratio_expected,
-    100.0 * amount_received / amount_expected AS ratio_received,
-    100.0 * amount_settled / amount_expected AS ratio_settled,
-    100.0 * amount_partial / amount_expected AS ratio_partial,
-    100.0 * amount_pending / amount_expected AS ratio_pending,
+    0.0 AS ratio_expected,
+    0.0 AS ratio_received,
+    0.0 AS ratio_settled,
+    0.0 AS ratio_partial,
+    0.0 AS ratio_pending,
     --
-    0.0 AS variation_expected, -- TODO
-    0.0 AS variation_received, -- TODO
-    0.0 AS variation_settled, -- TODO
-    0.0 AS variation_partial, -- TODO
-    0.0 AS variation_pending, -- TODO
+    0.0 AS variation_expected,
+    0.0 AS variation_received,
+    0.0 AS variation_settled,
+    0.0 AS variation_partial,
+    0.0 AS variation_pending,
     --
-    100.0 * n_received / n_expected AS payment_rate,
-    100.0 * n_units_rented / n_units_owned AS occupation_rate
-FROM summary_1, summary_2, summary_3, summary_4;
+    0.0 AS payment_rate,
+    0.0 AS occupation_rate
+FROM summary_1, summary_2, summary_3, summary_4, summary_5;
