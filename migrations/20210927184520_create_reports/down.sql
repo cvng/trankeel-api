5bec8d707b5007b945965ff99de1b4a87a3842fd
Cedric Vangout <mail@cvng.dev>
2021-09-29 00:12:41 -0400

    Update down.sql

--- migrations/20210927184520_create_reports/down.sql
+++ migrations/20210927184520_create_reports/down.sql
@@ -1,4 +1,6 @@
 DROP VIEW IF EXISTS reports;
 DROP FUNCTION IF EXISTS generate_summary_reports(current_month TIMESTAMPTZ);
+DROP FUNCTION IF EXISTS get_expected_rents(current_month TIMESTAMPTZ);
 DROP FUNCTION IF EXISTS variation(x NUMERIC, y NUMERIC);
-DROP TYPE IF EXISTS summary_reports CASCADE;
+DROP TYPE IF EXISTS summary_reports;
+DROP TYPE IF EXISTS expected_rents;
