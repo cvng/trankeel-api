970398b51fb0282334709f6300a0010f2b4fb725
Cedric Vangout <mail@cvng.dev>
2021-10-08 18:10:56 -0400

    Update locale.rs

--- packages/kit/src/locale.rs
+++ packages/kit/src/locale.rs
@@ -2,6 +2,12 @@ pub const DEFAULT_COUNTRY: &str = "FR";
 
 pub const DEFAULT_CURRENCY: &str = "EUR";
 
+pub const DEFAULT_OFFSET: i32 = 3600; // Without DST.
+
+pub fn default_tz() -> chrono::FixedOffset {
+    chrono::FixedOffset::east(DEFAULT_OFFSET)
+}
+
 pub fn text(key: &str) -> String {
     key.into()
 }
