e8d6373eceea90c52de26d5486ffa754d90b042f
Cedric Vangout <mail@cvng.dev>
2021-10-11 18:22:15 -0400

    Update discussion_create.rs

--- packages/lib/src/messaging/discussion_create.rs
+++ packages/lib/src/messaging/discussion_create.rs
@@ -4,17 +4,14 @@ use crate::Result;
 use piteo_core::database::Db;
 use piteo_data::Discussion;
 use piteo_data::DiscussionId;
-use piteo_data::DiscussionType;
 use piteo_data::PersonId;
-use piteo_data::SubjectId;
 use validator::Validate;
 
 #[derive(InputObject, Validate)]
 pub struct CreateDiscussionInput {
     pub initiator_id: PersonId,
     pub recipient_id: PersonId,
-    pub subject_id: Option<SubjectId>,
-    pub message: String,
+    pub message: Option<String>,
 }
 
 pub fn create_discussion_unauthenticated(
@@ -25,39 +22,25 @@ pub fn create_discussion_unauthenticated(
 
     let account = db.accounts().by_person_id(&input.recipient_id)?;
 
-    let type_ = get_discussion_type(db, &input.subject_id);
-
     let discussion = db.discussions().create(Discussion {
         id: DiscussionId::new(),
         created_at: Default::default(),
         updated_at: Default::default(),
         account_id: account.id,
         initiator_id: input.initiator_id,
-        subject_id: input.subject_id,
-        type_,
+        status: Default::default(),
     })?;
 
-    push_message(
-        db,
-        PushMessageInput {
-            discussion_id: discussion.id,
-            sender_id: input.initiator_id,
-            message: input.message,
-        },
-    )?;
+    if let Some(message) = input.message {
+        push_message(
+            db,
+            PushMessageInput {
+                discussion_id: discussion.id,
+                sender_id: input.initiator_id,
+                message,
+            },
+        )?;
+    }
 
     Ok(discussion)
-}
-
-fn get_discussion_type(db: &impl Db, subject_id: &Option<SubjectId>) -> DiscussionType {
-    match subject_id {
-        Some(subject_id) => {
-            if db.candidacies().by_id(subject_id).is_ok() {
-                DiscussionType::Candidacy
-            } else {
-                DiscussionType::default()
-            }
-        }
-        None => DiscussionType::default(),
-    }
 }
