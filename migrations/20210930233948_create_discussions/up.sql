9413b07eb00343b409e5e7a7cf9c11a8620121d5
Cedric Vangout <mail@cvng.dev>
2021-10-11 00:39:56 -0400

    Update up.sql

--- migrations/20210930233948_create_discussions/up.sql
+++ migrations/20210930233948_create_discussions/up.sql
@@ -1,11 +1,12 @@
+CREATE TYPE discussionstatus AS ENUM ('active', 'candidacy');
+
 CREATE TABLE discussions (
     id UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
     created_at TIMESTAMPTZ DEFAULT current_timestamp,
     updated_at TIMESTAMPTZ,
     account_id UUID NOT NULL REFERENCES accounts(id),
     initiator_id UUID NOT NULL REFERENCES persons(id),
-    subject_id UUID,
-    type TEXT NOT NULL
+    status DISCUSSIONSTATUS NOT NULL
 );
 
 SELECT manage_updated_at('discussions');
