3e2e60bb138725517ef1cb5183bac01863f9775c
Cedric Vangout <mail@cvng.dev>
2021-10-11 00:45:32 -0400

    Update up.sql

--- migrations/20210930234023_create_messages/up.sql
+++ migrations/20210930234023_create_messages/up.sql
@@ -4,7 +4,10 @@ CREATE TABLE messages (
     updated_at TIMESTAMPTZ,
     discussion_id UUID NOT NULL REFERENCES discussions(id) ON DELETE CASCADE,
     sender_id UUID NOT NULL REFERENCES persons(id),
-    content TEXT NOT NULL
+    content TEXT,
+    event_id UUID REFERENCES events(id)
 );
+
+ALTER TABLE messages ADD CONSTRAINT content_or_event_id_check CHECK (num_nonnulls(content, event_id) = 1);
 
 SELECT manage_updated_at('messages');
