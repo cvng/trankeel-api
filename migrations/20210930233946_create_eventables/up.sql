c8e53cb8dbb20d75dbc769c1f3e8bf4320ba4d64
Cedric Vangout <mail@cvng.dev>
2021-10-12 02:16:07 -0400

    Update up.sql

--- migrations/20210930233946_create_eventables/up.sql
+++ migrations/20210930233946_create_eventables/up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE eventables (
-    id UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+    id UUID NOT NULL PRIMARY KEY,
     file_id UUID REFERENCES files(id),
     rent_id UUID REFERENCES rents(id),
     payment_id UUID REFERENCES payments(id),
@@ -7,3 +7,12 @@ CREATE TABLE eventables (
 );
 
 ALTER TABLE eventables ADD CONSTRAINT eventables_check CHECK (num_nonnulls(file_id, rent_id, payment_id, candidacy_id) = 1);
+
+CREATE OR REPLACE FUNCTION eventables_set_id() RETURNS trigger AS $$
+BEGIN
+    NEW.id := coalesce(NEW.file_id, NEW.rent_id, NEW.payment_id, NEW.candidacy_id);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_id BEFORE INSERT OR UPDATE ON eventables FOR EACH ROW EXECUTE PROCEDURE eventables_set_id();
