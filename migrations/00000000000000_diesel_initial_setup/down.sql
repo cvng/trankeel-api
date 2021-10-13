bb7caa862813da76f5726e82f4f9ed48b1b86935
Cedric Vangout <mail@cvng.dev>
2021-10-13 16:19:57 -0400

    Update down.sql

--- migrations/00000000000000_diesel_initial_setup/down.sql
+++ migrations/00000000000000_diesel_initial_setup/down.sql
@@ -1,2 +1,4 @@
 DROP FUNCTION IF EXISTS manage_updated_at(_tbl regclass);
 DROP FUNCTION IF EXISTS diesel_set_updated_at();
+DROP FUNCTION IF EXISTS manage_id();
+DROP FUNCTION IF EXISTS app_set_id();
