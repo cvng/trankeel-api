9b2044104ecb9f7e39d881f0da07f30ef08f5ecc
Cedric Vangout <mail@cvng.dev>
2021-10-17 17:25:58 -0400

    flag for release

--- packages/api/src/main.rs
+++ packages/api/src/main.rs
@@ -11,10 +11,12 @@ use rocket::launch;
 fn rocket() -> _ {
     dotenv::dotenv().ok();
     trankeel_graphql::write_schema("schema.graphql").ok();
+    #[cfg(not(debug_assertions))]
     let _guard = sentry();
     server::server().unwrap()
 }
 
+#[allow(dead_code)]
 fn sentry() -> sentry::ClientInitGuard {
     sentry::init((
         std::env::var("SENTRY_DSN").expect("SENTRY_DSN"),
