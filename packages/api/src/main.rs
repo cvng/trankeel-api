b2fcf9eebd01f12452abb157d9a41046bf50c54c
Cedric Vangout <mail@cvng.dev>
2021-10-17 16:32:40 -0400

    feat(api): plug sentry

--- packages/api/src/main.rs
+++ packages/api/src/main.rs
@@ -11,5 +11,16 @@ use rocket::launch;
 fn rocket() -> _ {
     dotenv::dotenv().ok();
     trankeel_graphql::write_schema("schema.graphql").ok();
+    let _guard = sentry();
     server::server().unwrap()
+}
+
+fn sentry() -> sentry::ClientInitGuard {
+    sentry::init((
+        std::env::var("SENTRY_DSN").expect("SENTRY_DSN"),
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            ..Default::default()
+        },
+    ))
 }
