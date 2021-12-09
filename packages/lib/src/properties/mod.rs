deeae01cb2efd42b6113b19b98212817f998ec82
Cedric Vangout <mail@cvng.dev>
2021-12-09 17:00:38 -0400

    check ad updated

--- packages/lib/src/properties/mod.rs
+++ packages/lib/src/properties/mod.rs
@@ -4,6 +4,7 @@ mod property_delete;
 mod property_update;
 mod update_advertisement;
 
+pub use update_advertisement::*;
 pub use create_advertisement::*;
 pub use create_property::*;
 pub use property_delete::*;
