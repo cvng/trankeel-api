9dc424060826d388d21259f1875138fcdd4d445d
Cedric Vangout <mail@cvng.dev>
2021-12-12 18:59:14 -0400

    Update create_notices.rs

--- packages/lib/src/leases/create_notices.rs
+++ packages/lib/src/leases/create_notices.rs
@@ -3,12 +3,11 @@ use async_graphql::InputObject;
 use chrono::Utc;
 use trankeel_core::context::Context;
 use trankeel_core::database::Db;
-use trankeel_core::dispatcher::dispatch;
+use trankeel_core::dispatcher;
 use trankeel_core::dispatcher::Event;
 use trankeel_core::pdfmaker::Pdfmaker;
 use trankeel_core::templates::NoticeDocument;
 use trankeel_data::notice_filename;
-use trankeel_data::AuthId;
 use trankeel_data::DateTime;
 use trankeel_data::FileType;
 use trankeel_data::Notice;
@@ -23,92 +22,94 @@ pub struct CreateNoticesInput {
     date: Option<DateTime>,
 }
 
-pub(crate) async fn create_notices(
-    ctx: &Context,
-    auth_id: &AuthId,
-    input: CreateNoticesInput,
-) -> Result<Vec<Notice>> {
-    input.validate()?;
-
-    let rents = find_rents(ctx, auth_id, input.rent_ids)?;
-
-    let notices = generate_notices(ctx, rents).await?;
-
-    Ok(notices)
+pub struct CreateNoticesPayload {
+    pub notices: Vec<Notice>,
 }
 
-fn find_rents(ctx: &Context, _auth_id: &AuthId, rent_ids: Vec<RentId>) -> Result<Vec<Rent>> {
-    let db = ctx.db();
-
-    let mut rents = vec![];
-
-    for rent_id in rent_ids {
-        let rent = db.rents().by_id(&rent_id)?;
-        rents.push(rent);
-    }
-
-    Ok(rents)
+pub(crate) struct CreateNotices<'a> {
+    ctx: &'a Context,
 }
 
-async fn generate_notices(ctx: &Context, rents: Vec<Rent>) -> Result<Vec<Notice>> {
-    let db = ctx.db();
-    let pdfmaker = ctx.pdfmaker();
-
-    let mut notices = vec![];
-
-    for rent in rents {
-        // Try to fetch associated entities.
-        let lease = db.leases().by_id(&rent.lease_id)?;
-        let tenants = db.tenants().by_lease_id(&lease.id)?;
-        let property = db.properties().by_id(&lease.property_id)?;
-        let lender = db.lenders().by_id(&property.lender_id)?;
-
-        // Init new notice.
-        let notice_id = NoticeId::new();
-        let mut notice = Notice {
-            id: notice_id,
-            type_: FileType::PaymentNotice,
-            filename: Some(notice_filename(&notice_id, &rent)),
-            status: None,
-            external_id: None,
-            download_url: None,
-            preview_url: None,
-            created_at: None,
-            updated_at: None,
-        };
-
-        // Try to generate notice document (PDF).
-        let document = NoticeDocument::try_new(
-            notice.clone(),
-            rent.clone(),
-            lender,
-            tenants,
-            property,
-            Utc::now().into(),
-        )?;
-        let document = pdfmaker.generate(document).await?;
-
-        // Assign notice external ID.
-        notice.external_id = Some(document.id);
-        notice.status = Some(document.status);
-
-        // Create notice.
-        let notice = match db.files().create(&notice) {
-            Ok(notice) => notice,
-            Err(err) => return Err(err),
-        };
-
-        // Link notice with rent.
-        db.rents().update(&Rent {
-            id: rent.id,
-            notice_id: Some(notice.id),
-            ..rent
-        })?;
-
-        notices.push(notice.clone());
-
-        dispatch(ctx, vec![Event::NoticeCreated(notice)])?;
+impl<'a> CreateNotices<'a> {
+    pub fn new(ctx: &'a Context) -> Self {
+        Self { ctx }
     }
+}
 
-    Ok(notices)
+impl<'a> CreateNotices<'a> {
+    pub async fn run(self, input: CreateNoticesInput) -> Result<CreateNoticesPayload> {
+        let ctx = self.ctx;
+        let db = self.ctx.db();
+        let pdfmaker = self.ctx.pdfmaker();
+
+        input.validate()?;
+
+        // Find rents.
+        let mut rents = vec![];
+
+        for rent_id in input.rent_ids {
+            let rent = db.rents().by_id(&rent_id)?;
+            rents.push(rent);
+        }
+
+        // Generate notices.
+
+        let mut notices = vec![];
+
+        for rent in rents {
+            // Try to fetch associated entities.
+            let lease = db.leases().by_id(&rent.lease_id)?;
+            let tenants = db.tenants().by_lease_id(&lease.id)?;
+            let property = db.properties().by_id(&lease.property_id)?;
+            let lender = db.lenders().by_id(&property.lender_id)?;
+
+            // Init new notice.
+            let notice_id = NoticeId::new();
+            let mut notice = Notice {
+                id: notice_id,
+                type_: FileType::PaymentNotice,
+                filename: Some(notice_filename(&notice_id, &rent)),
+                status: None,
+                external_id: None,
+                download_url: None,
+                preview_url: None,
+                created_at: None,
+                updated_at: None,
+            };
+
+            // Try to generate notice document (PDF).
+            let document = NoticeDocument::try_new(
+                notice.clone(),
+                rent.clone(),
+                lender,
+                tenants,
+                property,
+                Utc::now().into(),
+            )?;
+            let document = pdfmaker.generate(document).await?;
+
+            // Assign notice external ID.
+            notice.external_id = Some(document.id);
+            notice.status = Some(document.status);
+
+            // Create notice.
+            let notice = match db.files().create(&notice) {
+                Ok(notice) => notice,
+                Err(err) => return Err(err),
+            };
+
+            // Link notice with rent.
+            db.rents().update(&Rent {
+                id: rent.id,
+                notice_id: Some(notice.id),
+                ..rent
+            })?;
+
+            notices.push(notice.clone());
+
+            dispatcher::dispatch(ctx, vec![Event::NoticeCreated(notice)])?;
+        }
+
+        Ok(CreateNoticesPayload { notices })
+    }
 }
