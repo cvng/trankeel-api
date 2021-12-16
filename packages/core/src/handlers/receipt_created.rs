8cd65349aefc47f0d462a9a7b7c49e1077bc5d3e
Cedric Vangout <mail@cvng.dev>
2021-12-15 22:05:05 -0400

    Update receipt_created.rs

--- packages/core/src/handlers/receipt_created.rs
+++ packages/core/src/handlers/receipt_created.rs
@@ -3,20 +3,34 @@ use crate::database::Db;
 use crate::dispatcher::Event;
 use crate::error::Result;
 use crate::messenger::Messenger;
+use trankeel_data::EventType;
 use trankeel_data::Eventable;
 use trankeel_data::Receipt;
 
-pub fn receipt_created(ctx: &Context, event: &Event, receipt: &Receipt) -> Result<()> {
+#[derive(Clone)]
+pub struct ReceiptCreated {
+    pub receipt: Receipt,
+}
+
+impl From<ReceiptCreated> for Event {
+    fn from(item: ReceiptCreated) -> Self {
+        Event::ReceiptCreated(item)
+    }
+}
+
+pub fn receipt_created(ctx: &Context, event: ReceiptCreated) -> Result<()> {
     let db = ctx.db();
     let messenger = ctx.messenger();
 
+    let ReceiptCreated { receipt } = event;
+
     let account = db.accounts().by_receipt_id(&receipt.id)?;
     let participant = db.persons().by_receipt_id(&receipt.id)?;
-    let eventable = db.eventables().create(&Eventable::File(receipt.clone()))?;
+    let eventable = db.eventables().create(&Eventable::File(receipt))?;
 
     messenger.message(
         db,
-        event.clone().into(),
+        EventType::ReceiptCreated,
         eventable.id(),
         account.id,
         participant.id,
