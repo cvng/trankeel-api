f55cadf35402156a39d6e8c86491920aecf5c252
Cedric Vangout <mail@cvng.dev>
2022-02-20 14:40:58 -0400

    Update guards.rs

--- packages/api/src/guards.rs
+++ packages/api/src/guards.rs
@@ -1,61 +1,117 @@
 use firebase_admin_auth_rs::jwk_auth::JwkAuth;
-use rocket::http::Status;
-use rocket::request::FromRequest;
-use rocket::request::Outcome;
-use rocket::Request;
+use poem::async_trait;
+use poem::error::Unauthorized;
+use poem::Endpoint;
+use poem::IntoResponse;
+use poem::Middleware;
+use poem::Request;
+use poem::Response;
+use poem::Result;
+use std::sync::Arc;
 use trankeel::config::Config;
 use trankeel::AuthId;
 
-const TOKEN_PREFIX: &str = "Bearer";
+const TOKEN_HEADER: &str = "Authorization";
+const TOKEN_SCHEME: &str = "Bearer";
 
-#[derive(Debug)]
+type Verifier = Arc<JwkAuth>;
+
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
-    Unauthorized,
+    #[error("error verifying ID token")]
+    InvalidToken,
 }
 
-/// Authorization request guard.
-///
-/// https://rocket.rs/v0.5-rc/guide/requests/#request-guards
-pub struct AuthGuard(Option<AuthId>);
+pub struct AuthIdGuard(Option<AuthId>);
 
-impl AuthGuard {
+impl AuthIdGuard {
+    pub fn new(auth_id: String) -> Self {
+        Self(Some(AuthId::new(auth_id)))
+    }
+
     pub fn inner(&self) -> Option<AuthId> {
         self.0.clone()
     }
 }
 
-#[rocket::async_trait]
-impl<'r> FromRequest<'r> for AuthGuard {
-    type Error = Error;
+/// A middleware that verify token from HTTP headers.
+///
+/// https://docs.rs/poem/latest/poem/middleware/trait.Middleware.html
+pub struct AuthMiddleware {
+    config: Config,
+    verifier: Verifier,
+}
 
-    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        let config = request.rocket().state::<Config>().unwrap();
-        let jwk_auth = request.rocket().state::<JwkAuth>().unwrap();
+impl AuthMiddleware {
+    pub async fn new(config: Config) -> Self {
+        let project_id = config.firebase_project_id.clone().unwrap();
 
-        let maybe_token = request.headers().get_one("authorization");
+        Self {
+            config,
+            verifier: Arc::new(JwkAuth::new(project_id).await),
+        }
+    }
+}
+
+impl<E: Endpoint> Middleware<E> for AuthMiddleware {
+    type Output = AuthMiddlewareImpl<E>;
+
+    fn transform(&self, endpoint: E) -> Self::Output {
+        AuthMiddlewareImpl {
+            endpoint,
+            config: self.config.clone(),
+            verifier: self.verifier.clone(),
+        }
+    }
+}
+
+pub struct AuthMiddlewareImpl<E> {
+    endpoint: E,
+    config: Config,
+    verifier: Verifier,
+}
+
+#[async_trait]
+impl<E: Endpoint> Endpoint for AuthMiddlewareImpl<E> {
+    type Output = Response;
+
+    async fn call(&self, mut request: Request) -> Result<Self::Output> {
+        let config = self.config.clone();
+        let verifier = self.verifier.clone();
+
+        let maybe_token = request
+            .header(TOKEN_HEADER)
+            .map(|token| token[TOKEN_SCHEME.len() + 1..].to_string());
 
         let auth_id = match maybe_token {
             Some(token) => {
                 // Verify ID token from request in production (release).
-                match jwk_auth.verify(&extract_token(token)) {
+                match verifier.verify(&token) {
                     Some(data) => data.claims.sub,
-                    _ => return Outcome::Failure((Status::Unauthorized, Error::Unauthorized)),
+                    _ => return Err(Unauthorized(Error::InvalidToken)),
                 }
             }
             None => {
                 // Try ID token fallback from env in development (debug).
-                match config.debug_auth_id.clone() {
-                    #[cfg(debug_assertions)]
+                match config.debug_auth_id {
+                    #[cfg(not(feature = "release"))]
                     Some(debug_auth_id) => debug_auth_id,
-                    _ => return Outcome::Success(Self(None)),
+                    _ => {
+                        return self
+                            .endpoint
+                            .call(request)
+                            .await
+                            .map(|response| response.into_response())
+                    }
                 }
             }
         };
 
-        Outcome::Success(Self(Some(AuthId::new(auth_id))))
-    }
-}
+        request.set_data(AuthIdGuard::new(auth_id));
 
-fn extract_token(token: &str) -> String {
-    token[TOKEN_PREFIX.len() + 1..].to_string()
+        self.endpoint
+            .call(request)
+            .await
+            .map(|response| response.into_response())
+    }
 }
